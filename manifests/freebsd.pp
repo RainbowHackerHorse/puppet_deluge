7e1a869ded0292e9c2a05fe78dfbe9253f9f2191
Rainbow <rainbow@sanitylinux.org>
2018-02-05 00:07:45 -0800

    Remove zfs resources from freebsd.pp

--- manifests/freebsd.pp
+++ manifests/freebsd.pp
@@ -37,28 +37,4 @@ class puppet_deluge::freebsd {
       enable  => true,
       require => File['/usr/local/etc/rc.d/deluged'],
   }
-  zfs::create { 'zroot/torrents':
-    ensure     => present,
-    filesystem => '/torrents',
-  }
-  zfs::create { 'zroot/torrents/downloading':
-    ensure     => present,
-    filesystem => '/torrents/downloading',
-    require    => Zfs::Create['zroot/torrents'],
-  }
-  zfs::create { 'zroot/torrents/files':
-    ensure     => present,
-    filesystem => '/torrents/files',
-    require    => Zfs::Create['zroot/torrents'],
-  }
-  zfs::create { 'zroot/torrents/completed':
-    ensure     => present,
-    filesystem => '/torrents/completed',
-    require    => Zfs::Create['zroot/torrents'],
-  }
-  exec { 'zfsperms':
-    command => 'chown -R deluge:deluge /torrents',
-    path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-    require => [Zfs::Create['zroot/torrents'], Zfs::Create['zroot/torrents/downloading'], Zfs::Create['zroot/torrents/files'], Zfs::Create['zroot/torrents/completed']],
-  }
 }
