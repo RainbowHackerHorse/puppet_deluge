89f7f23a588b8429178622832723c374388162e6
Rainbow <rainbow@sanitylinux.org>
2018-02-05 00:06:27 -0800

    Add zfs.pp and nozfs.pp

---
+++ manifests/nozfs.pp
@@ -0,0 +1,10 @@
+# Class: puppet_deluge::nozfs
+# Lightweight deluge module to install a headless deluge server
+# This module contains legacy directory specific functions.
+# Module written by Rainbow <rainbow@hacker.horse>
+# Released under the 2-clause BSD license.
+# Depends on puppetlabs/apt
+#
+class puppet_deluge::nozfs {
+  # resources
+}
---
+++ manifests/zfs.pp
@@ -0,0 +1,10 @@
+# Class: puppet_deluge::zfs
+# Lightweight deluge module to install a headless deluge server
+# This module contains ZFS specific functions.
+# Module written by Rainbow <rainbow@hacker.horse>
+# Released under the 2-clause BSD license.
+# Depends on puppetlabs/apt
+#
+class puppet_deluge::zfs {
+  # resources
+}
